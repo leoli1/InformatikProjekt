b2acf66fff51652193226664307c9fe26fa1d118
Leonard Tomczak <Leonard@Leonards-MBP.fritz.box>
2016-08-24 18:59:02 +0200

    erste engine sachen

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.*;
+
 
 public class Main {
 
+	public static ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
